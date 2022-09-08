a9fa7f4e276b0353db5379c8f1451b9a3a5be5d3
meisReal <simas.cesnakauskas@gmail.com>
2022-09-08 16:23:08 +0300

    Board has a function handleClick which is now responsible for creating X on square

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
         return (
             <button className="square"
-                onClick={() => this.setState({ value: 'X' })}
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
@@ -27,8 +21,15 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
 
     render() {
